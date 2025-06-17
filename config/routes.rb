9c81584ad4de41ee8c4c653454f82f9899b3ebad
imkmki <209921994+imkmki@users.noreply.github.com>
2025-06-17 17:21:13 +0000

    routes.rbのコメントを修正して意味を明確にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'homes#top' # トップページを books#index に設定
+  root 'homes#top' # トップページを books#top に設定
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
